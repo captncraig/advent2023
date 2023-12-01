6901253b6433409d22befc60210c586003b926a8
Craig Peterson <192540+captncraig@users.noreply.github.com>
2023-12-01 11:12:26 -0500

    day 1!

---
+++ dayn_test.go
@@ -0,0 +1,15 @@
+package advent
+
+import "testing"
+
+func DayNP1(input string) {
+
+}
+
+func DayNP2(input string) {
+
+}
+
+func TestDayN_Ex1(t *testing.T) {
+
+}
---
+++ utils.go
@@ -0,0 +1,7 @@
+package advent
+
+import "strings"
+
+func Lines(input string) []string {
+	return strings.Split(input, "\n")
+}
